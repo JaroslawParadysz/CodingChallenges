299e14e1cce12bea26b6908bb3e8c29c0cc03030
JaroslawParadysz <jaroslaw.paradysz@gmail.com>
2020-06-04 15:55:53 +0200

    Added first challenge

---
+++ Algorithms/Algorithms/CodingChallangeOne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    //Write a program which for N elemental array finds the largest even sum of K elements. For example:
+    //Array = [1, 2, 3]; K = 2; Bigest even sume = 4
+    class CodingChallangeOne
+    {
+
+    }
+}
