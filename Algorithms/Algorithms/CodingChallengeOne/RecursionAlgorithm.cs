1aa8fd91cddd2fede22fcc1ea66d0ed125728839
JaroslawParadysz <jaroslaw.paradysz@gmail.com>
2020-06-09 20:39:52 +0200

    Cleanup

--- Algorithms/Algorithms/CodingChallengeOne/RecursionAlgorithm.cs
+++ Algorithms/Algorithms/CodingChallengeOne/RecursionAlgorithm.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Algorithms.CodingChallangeOne
+﻿namespace Algorithms.CodingChallangeOne
 {
     public class RecursionAlgorithm : IAlgorithm
     {
